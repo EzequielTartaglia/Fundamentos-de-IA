315f8576a51a4165d15eab45d410decdd621d99d
EzequielTartaglia <ezequielmtartaglia@gmail.com>
2024-02-19 22:38:02 -0300

    Clase 5

---
+++ Clase 4 - Generative AI Design and Programming/script.js
@@ -0,0 +1,10 @@
+const project1 = document.getElementById("project1");
+const project2 = document.getElementById("project2");
+
+project1.addEventListener('click', () => {
+    alert("Hiciste click en el proyecto 1")
+})
+
+project2.addEventListener('click', () => {
+    alert("Hiciste click en el proyecto 2")
+})
